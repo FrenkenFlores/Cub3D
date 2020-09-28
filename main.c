04ce353e1840331e57936ea8f0d68d7d961e18c7
fflores <fflores@student.21-school.ru>
2020-09-28 21:55:47 +0300

    update #1

--- main.c
+++ main.c
@@ -1,6 +1,34 @@
 #include "cub3d.h"
 
+size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	if (n == 0)
+		return (0);
+	while (*s1 == *s2 && *s1 != '\0' && *s2 != '\0' && n > 1)
+	{
+	s1++;
+	s2++;
+	n--;
+	}
+	return (*(unsigned char*)s1 - *(unsigned char*)s2);
+}
+
 int	main(int argc, char **argv)
 {
+	t_data	data;
+
+	data.mlx = mlx_init();
+	data.mlx_win = mlx_new_window(data.mlx, W, H, "window");
+	mlx_loop(data.mlx);
 	return (0);
 }
